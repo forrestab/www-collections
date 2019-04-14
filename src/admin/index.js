f4496456f66e284284339c89d24f789db0e5843d
Forrest Brown <forrestabrown@gmail.com>
2019-04-13 23:29:52 -0400

    feat: add cloudinary media library plugin and config

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,1 +1,19 @@
-import CMS from "netlify-cms";
+window.CMS_MANUAL_INIT = true;
+
+import CMS, { init } from "netlify-cms";
+import cloudinaryMediaLibrary from "netlify-cms-media-library-cloudinary";
+
+init({
+	config: {
+		media_library: {
+			name: "cloudinary",
+			output_filename_only: true,
+			config: {
+				cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+				api_key: process.env.CLOUDINARY_API_KEY
+			}
+		}
+	}
+});
+
+CMS.registerWidget("cloudinary", cloudinaryMediaLibrary);
