89ffc8b3ea07793425581fa2f6a85e70034dcc6f
Forrest Brown <forrestabrown@gmail.com>
2019-06-22 21:50:59 -0400

    refactor: allow deck to project custom content

--- src/components/Deck.vue
+++ src/components/Deck.vue
@@ -1,29 +1,24 @@
 <template>
-	<section class="deck">
+	<div class="deck">
 		<h2 v-if="title">{{ title }}</h2>
-		<Card
-			v-for="item in items"
-			:key="item.id"
-			:title="item.title"
-			:data="item"
-		/>
-	</section>
+		<template v-for="item in items">
+			<slot :data="item" />
+		</template>
+	</div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
-
 <script>
-import Card from "~/components/Card.vue";
-
 export default {
-	components: {
-		Card
-	},
+	name: "CDeck",
 	props: {
 		title: { type: String },
 		items: { type: Array }
 	}
 }
 </script>
+
+<style lang="scss" scoped>
+.deck {
+
+}
+</style>
